9b69d81f3553d6277b9d768b3443f5ee742e0462
jaehong-park-78 <60995017+jaehong-park-78@users.noreply.github.com>
2020-03-06 18:42:11 +0900

    Add files via upload

---
+++ body/ban.py
@@ -0,0 +1,17 @@
+moto='simply and neatly for fantastic'
+license='Simplified BSD'
+papaver='2020.03.05_v0.1'
+sonver='2020.03.07_v0.5'
+banner='''
+       [ mr646464@kakao.com ]
+ ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀▀▀▄   ▄▀▀▄▀▀▀▄ 
+█      █ █      █ █      █ █   █   █ 
+█      █ █      █ █      █ ▐  █▀▀▀▀  
+ ▀▄▄▄▄▀▄ ▀▄    ▄▀ ▀▄    ▄▀    █      
+        █  ▀▀▀▀     ▀▀▀▀    ▄▀       
+        ▐                  █         
+                           ▐  Scanner '''+sonver+'\n\n'
+usauge='''Usauge : ./qoop_scan -h(ost) ipaddress -m(odule) purpose 
+Example: ./qoop_scan -h 192.168.1.1 -m ping''' 
+
+defaulta=banner+usauge
